9477db465616eb02831a3b22759dc941067fc5cb
ptrkc <40273827+ptrkc@users.noreply.github.com>
2021-07-12 15:06:24 -0300

    Addind new items using the API

--- src/components/InsertForm.js
+++ src/components/InsertForm.js
@@ -1,36 +1,44 @@
+import axios from "axios";
 import { useState } from "react";
 import styled from "styled-components";
 
 export default function InsertForm({ onAddItem }) {
-  const [text, setText] = useState("");
+    const [text, setText] = useState("");
 
-  function handleSubmit(e) {
-    e.preventDefault();
+    function handleSubmit(e) {
+        e.preventDefault();
 
-    const newItem = { text };
-    // Save item to server
+        const newItem = { text };
+        const itemsRequest = axios.post(
+            `${process.env.REACT_APP_API_URL}/items`,
+            newItem
+        );
+        itemsRequest.then(() => {
+            setText("");
+            onAddItem();
+        });
+        itemsRequest.catch(() => {
+            alert("Error!");
+        });
+    }
 
-    setText("");
-    onAddItem();
-  }
-
-  return (
-    <form onSubmit={handleSubmit}>
-      <Input
-        onChange={(e) => setText(e.target.value)}
-        value={text}
-        placeholder="Insert a new item..."
-      />
-    </form>
-  );
+    return (
+        <form onSubmit={handleSubmit}>
+            <Input
+                onChange={(e) => setText(e.target.value)}
+                value={text}
+                placeholder="Insert a new item..."
+            />
+        </form>
+    );
 }
 
 const Input = styled.input`
-  width: 400px;
-  line-height: 40px;
-  font-size: 20px;
-  border: none;
-  border-radius: 10px;
-  padding: 0 10px;
-  outline: none;
+    width: 400px;
+    line-height: 40px;
+    font-size: 20px;
+    border: none;
+    border-radius: 10px;
+    padding: 0 10px;
+    outline: none;
 `;
