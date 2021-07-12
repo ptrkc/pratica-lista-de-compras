ffc28662f74132205c342e6c8693ba3169e62251
ptrkc <40273827+ptrkc@users.noreply.github.com>
2021-07-12 15:00:16 -0300

    Gettings items from API

--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,41 +1,45 @@
+import axios from "axios";
 import { useEffect, useState } from "react";
 import styled from "styled-components";
 import InsertForm from "./InsertForm";
 
 export default function ShoppingList() {
-  // Fake data
-  const [items, setItems] = useState([
-    { id: 1, text: "Pão" },
-    { id: 2, text: "Salsicha" },
-    { id: 3, text: "Ketchup" },
-  ]);
+    const [items, setItems] = useState([]);
 
-  useEffect(loadItems, []);
+    useEffect(loadItems, []);
 
-  function loadItems() {
-    // Get items from back-end and update state
-  }
+    function loadItems() {
+        const itemsRequest = axios.get(
+            `${process.env.REACT_APP_API_URL}/items`
+        );
+        itemsRequest.then((response) => {
+            setItems(response.data);
+        });
+        itemsRequest.catch((error) => {
+            alert("Error!");
+        });
+    }
 
-  return (
-    <>
-      <InsertForm onAddItem={loadItems} />
-      <List>
-        {items.map((item) => (
-          <li key={item.id}>{item.text}</li>
-        ))}
-      </List>
-    </>
-  );
+    return (
+        <>
+            <InsertForm onAddItem={loadItems} />
+            <List>
+                {items.map((item) => (
+                    <li key={item.id}>{item.text}</li>
+                ))}
+            </List>
+        </>
+    );
 }
 
 const List = styled.ul`
-  margin-top: 40px;
-  background: #fff;
-  width: 600px;
-  padding: 20px;
-  border-radius: 10px;
-  font-size: 25px;
-  padding-left: 50px;
-  line-height: 40px;
-  list-style-type: disc;
+    margin-top: 40px;
+    background: #fff;
+    width: 600px;
+    padding: 20px;
+    border-radius: 10px;
+    font-size: 25px;
+    padding-left: 50px;
+    line-height: 40px;
+    list-style-type: disc;
 `;
